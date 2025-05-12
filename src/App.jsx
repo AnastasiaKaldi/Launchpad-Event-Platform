e0324d3c1fd172fcb47ee112aabad70daf4effa4
AnastasiaKaldi <kaldianastasia@gmail.com>
2025-05-12 17:26:05 +0100

    Setting up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-purple-500">
+      {/* Your content can go here */}
+    </div>
+  );
+}
+
+export default App;
