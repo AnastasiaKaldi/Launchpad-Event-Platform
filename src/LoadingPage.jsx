0eb3e02c6bf39a0d810d20f4efc6a31fc32a42d6
AnastasiaKaldi <kaldianastasia@gmail.com>
2025-06-03 12:52:46 +0100

    Nelify setup

--- src/LoadingPage.jsx
+++ src/LoadingPage.jsx
@@ -1,11 +1,25 @@
 // LandingPage.jsx
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Homepage from "./Homepage";
 import EventsPage from "./EventsPage";
 import CreateEvent from "../src/Createevent";
 import Footer from "./Footer";
 
 const LandingPage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const el = document.querySelector(location.hash);
+      if (el) {
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+    }
+  }, [location]);
+
   return (
     <div className="overflow-x-hidden bg-[#dbd5c5]">
       <Homepage />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -87,23 +87,19 @@ function Navbar() {
           className="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start"
         >
           <ul className="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
-            <li className="md:mr-12">
-              <a
-                className="text-[#dbd5c5] hover:text-[#a79066]"
-                style={{ fontFamily: "Inknut Antiqua" }}
-                href="#events"
-              >
-                Events
-              </a>
+            <li
+              className="md:mr-12 text-[#dbd5c5] hover:text-[#a79066]"
+              style={{ fontFamily: "Inknut Antiqua" }}
+            >
+              <Link to={{ pathname: "/", hash: "#events" }}>Events</Link>
             </li>
-            <li className="md:mr-12">
-              <a
-                className="text-[#dbd5c5] hover:text-[#a79066]"
-                style={{ fontFamily: "Inknut Antiqua" }}
-                href="#create-events"
-              >
+            <li
+              className="md:mr-12 text-[#dbd5c5] hover:text-[#a79066]"
+              style={{ fontFamily: "Inknut Antiqua" }}
+            >
+              <Link to={{ pathname: "/", hash: "#create-events" }}>
                 Create Events
-              </a>
+              </Link>
             </li>
 
             {!user ? (
